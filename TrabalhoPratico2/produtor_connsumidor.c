d64ea5a374f15c290815421c6af94fab11dfc003
patykov <patriciakovaleski@gmail.com>
2016-05-14 23:14:49 -0300

    Almost working

--- TrabalhoPratico2/produtor_connsumidor.c
+++ TrabalhoPratico2/produtor_connsumidor.c
@@ -6,22 +6,30 @@
 #include <semaphore.h>
 
 #define LIMIT 10000000
-#define MAXIMUM_NUMBER 10000 //Max number of products consumed. (stop condition)
+#define MAXIMUM_NUMBER 50 //Max number of products consumed. (stop condition)
 
 int M; //Counter to the number of products consumed 
 long int N; //Size of shared memory vector
 long int *vector = NULL;
+sem_t *sem_mutex;
+sem_t *sem_full;
+sem_t *sem_empty;
 
 void *num_generator(void *threadid){
-	long int n = (rand() % LIMIT);
+	long int n;
+	long tid = (long)threadid;
 	while(M != MAXIMUM_NUMBER){
+		n = (rand() % LIMIT);
 		//Produce
 		sem_wait(&sem_empty);
 		sem_wait(&sem_mutex);
 		//Adding resource to the vector
 		for (int i=0; i<N; i++){
 			if (vector[i] == 0){
+				printf("Thread %d produtora adicionando o numero: %d \n", tid, n );
 				vector[i] = n;
+				printf("vector[%d, %d, %d, %d]\n", vector[0],vector[1],vector[2],vector[3]);
+				sleep(1);
 				break;
 			}
 		}
@@ -31,16 +39,17 @@ void *num_generator(void *threadid){
 }
 
 void *num_avaliator(void *threadid){
-		while(M != MAXIMUM_NUMBER){
+	long tid = (long)threadid;
+	while(M != MAXIMUM_NUMBER){
 		//Produce
 		sem_wait(&sem_full);
 		sem_wait(&sem_mutex);
-		//Adding resource to the vector
-		for (int i=0; i<N; i++){
+		int flag = 0;
+		//Consume an item
+		for (int i=(N-1); i>=0; i--){
 			long int n = vector[i];
 			if (n != 0){
 				//Check if it's a prime number
-				int flag = 0;
     			for (int i=2; i<=n/2; ++i){
         			if (n%i == 0){
             			flag = 1;
@@ -48,12 +57,14 @@ void *num_avaliator(void *threadid){
         			}
     			}
     			vector[i] = 0;
-    			sem_post(&sem_mutex);
-				sem_post(&sem_empty);
+    			M++;
 				if(flag == 0)
-        			printf("%ld is a prime number.\n", n);
+        			printf("%ld is a prime number. Removed from vector[%d] Thread %d\n", n, i, tid);
     			else
-        			printf("%ld is not a prime number.\n", n);
+        			printf("%ld is not a prime number.Removed from vector[%d] Thread %d\n", n, i, tid);
+        		sleep(1);
+        		sem_post(&sem_mutex);
+				sem_post(&sem_empty);
     			break;
 			}
 		}
@@ -81,12 +92,9 @@ int main(int argc, char *argv[]){
     long t;
  	int err;
 
-	sem_t *sem_mutex;
-	sem_t *sem_full;
-	sem_t *sem_empty;
 	sem_init(&sem_mutex, 0, 1);
-	sem_init(&sem_full, 0, 1);
-	sem_init(&sem_empty, 0, 1);
+	sem_init(&sem_full, 0, 0);
+	sem_init(&sem_empty, 0, N);
 
 
  	//Generate random seed
