20595da5d98925abf187d52231eba6b32a41532c
patykov <patriciakovaleski@gmail.com>
2016-07-04 01:32:13 -0300

    Finalizando primeira parte do trabalho 3

--- TrabalhoPratico3/client.py
+++ TrabalhoPratico3/client.py
@@ -1,6 +1,91 @@
 import xmlrpclib
+import sys
+import time
+import math
+from threading import Thread
+from datetime import datetime, timedelta
+from random import randrange
 
-proxy = xmlrpclib.ServerProxy('http://localhost:8000')
-print(proxy.pow(2,3))  # Returns 2**3 = 8
-print(proxy.add(2,3))  # Returns 5
-print(proxy.mul(5,2))  # Returns 5*2 = 10
+N = 10 #10^8
+vector = [0]*N
+
+if len(sys.argv) != 4:
+    print "Verify the functions parameters.\n"
+    exit(1)
+else:
+	K = int(sys.argv[1])
+	OP = int(sys.argv[2])  # 1 = Add; 2 = Pow; 3 = Mul
+	X = int(sys.argv[3])
+
+
+mdelay = timedelta(0)
+tempo1 = datetime.now()
+
+def divide_vector(n, k):
+	n_p_threads = math.ceil(n/k)
+	if (n%k != 0):
+		n_p_threads += 1
+	return int(n_p_threads)
+
+def add_random_number(inicio, fim):
+	for i in range(inicio, fim):
+		vector[i] = randrange(0, 100)
+
+def rpc_call(inicio, fim, x):
+	server = xmlrpclib.ServerProxy('http://localhost:8000')
+	if(OP == 1):
+		for i in range(inicio, fim):
+			vector[i] = server.add(vector[i], x)
+	elif(OP == 2):
+		for i in range(inicio, fim):
+			vector[i] = server.pow(vector[i], x)
+	elif(OP == 3):
+		for i in range(inicio, fim):
+			vector[i] = server.mul(vector[i], x)
+	else:
+		print "Operation not valid! Please select: 1 = Add; 2 = Pow; 3 = Mul."
+
+number_per_threads = divide_vector(N, K)
+i = 0
+threads = []
+
+#Populate the vector with random numbers
+while(i<N):
+	f = i + number_per_threads
+	if(f>=N):
+		f = N
+	t = Thread(target=add_random_number, args=(i, f))
+	t.start()
+	i += number_per_threads
+	threads.append(t)
+
+for t in threads:
+	t.join()
+
+del threads[:]
+
+for i in vector:
+	print i
+
+i = 0
+#Call rpc function for each thread
+while(i<N):
+	f = i + number_per_threads
+	if(f>=N):
+		f = N
+	t = Thread(target=rpc_call, args=(i, f, X))
+	t.start()
+	i += number_per_threads
+	threads.append(t)
+
+for t in threads:
+	t.join()
+
+print "_______________"
+for i in vector:
+	print i
+
+tempo2 = datetime.now()   
+mdelay = mdelay + (tempo2 - tempo1)
+print mdelay
+
--- TrabalhoPratico3/server.py
+++ TrabalhoPratico3/server.py
@@ -1,34 +1,22 @@
 from SimpleXMLRPCServer import SimpleXMLRPCServer
-import logging
-import os
 
-# Set up logging
-logging.basicConfig(level=logging.DEBUG)
 
-server = SimpleXMLRPCServer(('localhost', 8000), logRequests=True)
-
-# Expose a function
-def list_contents(dir_name):
-    logging.debug('list_contents(%s)', dir_name)
-    return os.listdir(dir_name)
-server.register_function(list_contents)
+server = SimpleXMLRPCServer(("localhost", 8000))
 
 server.register_function(pow)
 
-# Register a function under a different name
 def adder_function(x,y):
     return x + y
 server.register_function(adder_function, 'add')
 
-# Register an instance; all the methods of the instance are
-# published as XML-RPC methods (in this case, just 'mul').
-class MyFuncs:
-    def mul(self, x, y):
+def mul_function(x, y):
         return x * y
-server.register_instance(MyFuncs())
+server.register_function(mul_function, 'mul')
+
 
 try:
     print 'Use Control-C to exit'
     server.serve_forever()
 except KeyboardInterrupt:
+	
     print 'Exiting'
