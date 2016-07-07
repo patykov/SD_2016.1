757c2feeb24f87f0b0ba4406ad23a51fc57c4436
patykov <patriciakovaleski@gmail.com>
2016-07-06 23:35:45 -0300

    Attempt to do the centralized algorithm on the second exercise

--- TrabalhoPratico3/server2.py
+++ TrabalhoPratico3/server2.py
@@ -5,23 +5,27 @@ server = SimpleXMLRPCServer(("localhost", 8000))
 queue = []
 
 def receive_msg(msg, myTime, myId):
-	#REQUEST
-	if (msg == 1):
-		if(len(queue) == 0):
-			ENVIA GRANT
-		queue.append(myTime)
-
-	#RELEASE
-	elif(msg == 2):
-		queue.remove(myTime)
+	while(1):
 		if (len(queue) > 0):
-			ENVIA GRANT PARA queue[0].
+			if(myId == queue[0]):
+				return GRANT
 
-	else
-		print "Error"
-		exit(1)
+		#REQUEST
+		if (msg == 1):
+			if(len(queue) == 0):
+				return GRANT
+			queue.append(myId)
+
+		#RELEASE
+		elif(msg == 2):
+			remove a posicao 0 da lista e empurra geral uma posicao pra frente
+			return BELEZINHA
+			
+		else
+			print "Error"
+			exit(1)
+
     
-    return 
 server.register_function(receive_msg, 'send_msg')
 
 
